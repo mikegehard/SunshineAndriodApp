d8f81555a7127d93bdce31483469fe8a5a0f80f4
Mike Gehard <mike.gehard@gmail.com>
2014-07-27 14:43:12 -0600

    Get the app working

--- app/src/main/java/com/github/msgehard/sunshine/app/MainActivity.java
+++ app/src/main/java/com/github/msgehard/sunshine/app/MainActivity.java
@@ -1,18 +1,23 @@
 package com.github.msgehard.sunshine.app;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -48,40 +53,5 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            List<String> weatherData = new ArrayList<String>();
-
-            weatherData.add("Today - Sunny - 88/63");
-            weatherData.add("Tomorrow - Foggy - 70/46");
-            weatherData.add("Weds - Sunny - 88/63");
-            weatherData.add("Thurs - Sunny - 88/63");
-            weatherData.add("Fri - Sunny - 88/63");
-            weatherData.add("Sat - Sunny - 88/63");
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weatherData
-            );
-            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
-
-            listView.setAdapter(adapter);
-            return rootView;
-        }
     }
 }
